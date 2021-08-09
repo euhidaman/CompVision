490a0bc154ec95bb0decd91f20fa034291cb34cf
Euhid Aman <aman.derax20@gmail.com>
2021-08-09 21:05:04 +0530

    added short comments for step-by-step code

--- Desc.py
+++ Desc.py
@@ -20,6 +20,39 @@ def show_image_analysis(image_path, analysis):
                 " '{}'\n(Confidence: {:.2f}%)".format(
                     caption.text, caption.confidence * 100)
     plt.title(caption_text)
+    # -----------------
+
+    # Or
+
+    # Get objects
+    if analysis.objects:
+        # Draw a rectangle around each object
+        for object in analysis.objects:
+            r = object.rectangle
+            bounding_box = ((r.x, r.y), (r.x + r.w, r.y + r.h))
+            draw = ImageDraw.Draw(img)
+            draw.rectangle(bounding_box, outline='magenta', width=5)
+            plt.annotate(object.object_property, (r.x, r.y),
+                         backgroundcolor='magenta')
+
+    # ------------------
+
+    # Or
+
+    # Get faces
+    if analysis.faces:
+        # Draw a rectangle around each face
+        for face in analysis.faces:
+            r = face.face_rectangle
+            bounding_box = ((r.left, r.top),
+                            (r.left + r.width, r.top + r.height))
+            draw = ImageDraw.Draw(img)
+            draw.rectangle(bounding_box, outline='lightgreen', width=5)
+            annotation = 'Person aged approxilately {}'.format(face.age)
+            plt.annotate(annotation, (r.left, r.top),
+                         backgroundcolor='lightgreen')
+
+    # ------------------
 
     # Remove the Axes and Show the image
     plt.axis('off')
